8d8c8e20a02b325c8f613f3cf6c2bb6718d05d58
Tania-01 <tchirska@gmail.com>
2020-07-17 15:05:31 +0300

    new

---
+++ уроки/main.js
@@ -0,0 +1,49 @@
+// let day ;
+// let sunny ;
+// let rainy ;
+// let cold;
+// let weekend ;
+// function analyze() {
+//
+//     let output="";
+//     day =document.getElementById("check1").checked;
+//     sunny =document.getElementById("check2").checked;
+//      rainy =document.getElementById("check3").checked;
+//      cold=document.getElementById("check4").checked;
+//      weekend =document.getElementById("check5").checked;
+//
+// if (day===true){
+//     if (sunny===true)
+//     {
+//         output ="take your sun glasses"
+//
+//     }
+//     if( rainy===true){
+//         output +="take your umbrella"
+//     }
+//     if(cold===true){
+//         output+="take your coat"
+//     }
+//     if(weekend===true){
+//         output+="and finally go to picknick"
+//     }else{
+//         output+="go to your work"
+//     }
+// }
+// else {
+//     if( rainy===true){
+//         output +="take your umbrella"
+//     }
+//     if(cold===true){
+//         output+="take your coat"
+//     }
+//     if(weekend===true){
+//         output+="and finally go to cafe"
+//     }else{
+//         output+="go to your home ant taake some slep"
+//     }
+// }
+// alert(output);
+// }
+let numbers=[3,6,9,34,6,12,33]
+for( let i=0;)
